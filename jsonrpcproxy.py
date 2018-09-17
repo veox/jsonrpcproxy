3d092c055430b1d7b7c2646cf65be1302f59b7fd
Noel Maersk <veox@users.noreply.github.com>
2018-09-17 12:52:38 +0300

    jsonrpcproxy: all changes as-they-were to work with Trinity.

--- jsonrpcproxy.py
+++ jsonrpcproxy.py
@@ -21,6 +21,7 @@ from urllib.parse import urlparse
 import errno
 import socket
 import sys
+import json
 
 if sys.platform == 'win32':
     import win32file
@@ -28,7 +29,7 @@ if sys.platform == 'win32':
 
 
 VERSION = '0.1.0a1'
-BUFSIZE = 32
+BUFSIZE = 4096
 DELIMITER = ord('\n')
 INFO = """JSON-RPC Proxy
 
@@ -67,6 +68,7 @@ class UnixSocketConnector(object):
                 # Assign last, to keep it None in case of exception.
                 self._socket = s
             except OSError as ex:
+                print('>>> USC OSError!')
                 msg = self._get_error_message(ex.errno)
                 err = BackendError(msg.format(self._socket_path))
                 raise err from ex
@@ -85,14 +87,19 @@ class UnixSocketConnector(object):
         return self.socket().recv(max_length)
 
     def sendall(self, data):
+        print('>>> in USC sendall()')
         try:
-            return self.socket().sendall(data)
+            resp = self.socket().sendall(data)
+            print('>>> USC sendall() completed successfully')
+            return resp
         except OSError as ex:
+            print('>>> USC sendall() OSError')
             if ex.errno == errno.EPIPE:
-                # The connection was terminated by the backend. Try reconnect.
+                print('>>> The connection was terminated by the backend. Try reconnect.')
                 self.close()
                 return self.socket().sendall(data)
             else:
+                print('>>> USC sendall() OSError !EPIPE - raising')
                 raise
 
 
@@ -158,12 +165,29 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         request_length = int(self.headers['Content-Length'])
         request_content = self.rfile.read(request_length)
-        # self.log_message("Headers:  {}".format(self.headers))
-        # self.log_message("Request:  {}".format(request_content))
+        self.log_message('>>> HTTP req handler do_POST():')
+        self.log_message("Headers:\n{}".format(self.headers))
+        self.log_message("Request:  {}".format(request_content))
+        # eth_syncing has additional wrapping of [] X_X
+        if request_content[0] == ord(b'[') and request_content[-1] == ord(b']'):
+            self.log_message('>>> FUCK YOU JAVASCRIPT AND YOUR HELL-KNOWS-WHERE-THIS-CAME-FROM')
+            request_content = request_content[1:-1]
+            self.log_message("RequestNU:{}".format(request_content))
+        self.log_message('>>> ---------------- END request --------------')
 
         try:
-            response_content = self.server.process(request_content)
-            # self.log_message("Response: {}".format(response_content))
+            # if it's a request for "syncing", just short-circuit:
+            # web3.js says "method not implemented" is an invalid response X_X
+            if str(request_content).find('eth_syncing') != -1:
+                self.log_message('>>> fiddling with eth_syncing')
+                request_id = json.loads(str(request_content))['id']
+                response_content = bytes(
+                    '{"jsonrpc":"2.0", "result":false, "id":' + request_id + '}',
+                    encoding='utf-8')
+            else:
+                response_content = self.server.process(request_content)
+
+            self.log_message("Response: {}".format(response_content))
 
             self.send_response(200)
             self.send_header("Content-type", "application/json")
@@ -172,6 +196,7 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(response_content)
         except BackendError as err:
+            self.log_message('>>> HTTP req handler do_post() backend error!')
             self.send_response(502)
             error_msg = str(err).encode('utf-8')
             # TODO: Send as JSON-RPC response
@@ -180,14 +205,32 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(error_msg)
             self.log_message("Backend Error: {}".format(err))
-
-        # TODO: Handle other excaptions as error 500.
+        except Exception as err:
+            self.log_message('>>> HTTP req handler do_post() other exception!')
+            self.send_response(500)
+            error_msg = str(err).encode('utf-8')
+            # TODO: Send as JSON-RPC response
+            self.send_header("Content-type", "text/plain")
+            self.send_header("Content-length", len(error_msg))
+            self.end_headers()
+            self.wfile.write(error_msg)
+            self.log_message("Some Other Error: {}".format(err))
 
     def addCORS(self):
         self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
         self.send_header("Access-Control-Allow-Headers", "content-type")
 
+
+# A valid JSON RPC response can only end in } or ] http://www.jsonrpc.org/specification
+# (Copy-pasta from https://github.com/ethereum/web3.py/blob/master/web3/providers/ipc.py)
+def has_valid_json_rpc_ending(raw_response):
+    stripped_raw_response = raw_response.rstrip()
+    for valid_ending in [b"}", b"]"]:
+        if stripped_raw_response.endswith(valid_ending):
+            return True
+    else:
+        return False
 
 class Proxy(HTTPServer):
 
@@ -203,18 +246,35 @@ class Proxy(HTTPServer):
         self.conn = get_ipc_connector(self.backend_address)
 
     def process(self, request):
+        print('>>> Proxy process() request:', request)
         self.conn.sendall(request)
+        print('>>> Proxy process() request sent')
 
+        # OH LORD
         response = b''
         while True:
+            print('>>> in loop')
             r = self.conn.recv(BUFSIZE)
+            print('>>> recv done')
             if not r:
+                print('>>> not r')
                 break
             if r[-1] == DELIMITER:
+                print('>>> r last char delim')
                 response += r[:-1]
                 break
             response += r
+            print('>>> len(response) so far:', len(response))
+            print('>>> response so far     :', str(response))
+            if has_valid_json_rpc_ending(response):
+                # exit if valid JSON, continue if not (worst-case: timeout while in loop)
+                try:
+                    json.loads(response)
+                    break
+                except ValueError:
+                    continue
 
+        print('>>> Proxy process() response:', response)
         return response
 
 
